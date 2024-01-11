91103ae4276ded755349139d93f2609513b0f1b5
Natasha2027 <154408228+Natasha2027@users.noreply.github.com>
2024-01-11 14:50:38 +0300

    Update Pie.java

--- Lab1/src/bsu/rfe/java/group7/lab1/nazarenko/varA2/Pie.java
+++ Lab1/src/bsu/rfe/java/group7/lab1/nazarenko/varA2/Pie.java
@@ -2,30 +2,22 @@ package bsu.rfe.java.group7.lab1.nazarenko.varA2;
 
 public class Pie extends Food{
     private String filling;
-
+    
     public Pie(String filling) {
         super("Пирог");
         this.filling = filling;
     }
-
+    
     public void consume() {
         System.out.println(this + " съедено");
     }
-
+    
     public String getFilling() {
         return filling;
     }
 
     public void setFilling(String filling) {
         this.filling = filling;
-    }
-
-    public boolean equals(Object arg0) {
-        if (super.equals(arg0)) {
-            if (!(arg0 instanceof Pie)) return false;
-            return filling.equals(((Pie)arg0).filling);
-        } else
-            return false;
     }
 
     public String toString() {
